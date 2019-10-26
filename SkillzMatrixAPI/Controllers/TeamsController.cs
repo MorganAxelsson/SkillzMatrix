39071a72ce9f65e360b29d83ead0e9a1452b7207
Morgan Axelsson <morgan8339@gmail.com>
2019-10-26 23:37:28 +0200

    added Teamscontroller

---
+++ SkillzMatrixAPI/Controllers/TeamsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SkillzMatrixAPI.Data.Entities;
+using SkillzMatrixAPI.Data.Repositorys.Interfaces;
+
+namespace SkillzMatrixAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        private ITeamsRepository _TeamsRepository;
+        public TeamsController(ITeamsRepository teamsRepository)
+        {
+            _TeamsRepository = teamsRepository;
+        }
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var teams =  await _TeamsRepository.GetAll();
+
+            if (teams != null)
+                return Ok(teams);
+
+            return NotFound();
+        }
+    }
+}

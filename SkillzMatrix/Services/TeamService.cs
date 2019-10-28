306b9f260fdff0efd9e1a1c49fd25f4922c2956a
Morgan Axelsson <morgan8339@gmail.com>
2019-10-28 11:46:33 +0100

    adding teamservice and model

---
+++ SkillzMatrix/Services/TeamService.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Shared.Models;
+using SkillzMatrix.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SkillzMatrix.Services
+{
+    public class TeamService
+    {
+        public async Task<TeamModel[]> GetAllTeamsAsync()
+        {                    
+            HttpClient client = new HttpClient();
+            var result = await client.GetStringAsync("https://localhost:44344/teams/getall");
+            return JsonConvert.DeserializeObject<TeamModel[]>(result);
+        }
+    }
+}
